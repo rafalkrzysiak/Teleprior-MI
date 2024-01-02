92d73a7d8882d3cd0f907ec98b1333b20982cb91
Rafal <66642121+rafalkrzysiak@users.noreply.github.com>
2024-01-02 10:52:33 -0600

    code update

--- sar253/PostProcessSimExperiment.m
+++ sar253/PostProcessSimExperiment.m
@@ -57,12 +57,14 @@ conditions = ["condition_1",...
               "condition_2",...
               "condition_3",...
               "condition_4"];
+          
+cond_names=["xMxT","xMyT","yMxT","yMyT"];
 
 % -- define the mat file name used for all participants
 Matfile = "OmronLab_p=1200_nsim=1_agents=3.mat";
 
 % -- pull list of test folders within test folder
-parentDir = "data/test";
+parentDir = "data/alpha_t/TotalDist";
 parentDir_RW = "data/RandomWalk";
 files = dir(parentDir);
 RW_files = dir(parentDir_RW);
@@ -75,86 +77,143 @@ figure(1); clf; gcf;
 L= [18 9];
 
 % -- begin looping through each test folder
-% for test = 4:4%1:size(files, 1)
-% 
-%     % -- make sure that we capture a number not '.' or '..'
-%     if (files(test).name ~= "." && files(test).name ~= "..")
-% 
-%         % -- within the test number folder, get the participant numbers
-%         subDir = strcat(parentDir, "/", files(test).name);
-%         participant_folders = dir(subDir);
-% 
-%         % -- begin looping through all the participant folders
-%         for participant = 1:size(participant_folders, 1)
-%             figure(participant); gcf;
-%             % -- make sure that we capture a number not '.' or '..'
-%             if (participant_folders(participant).name ~= "." && participant_folders(participant).name ~= "..")
-% 
-%                 % -- create participant directory
-%                 partDir = strcat(subDir,"/",participant_folders(participant).name);
-% 
-%                 % -- begin looping through all conditions tested
-%                 for cond = 1:size(conditions, 2)
-% 
-%                     % -- create condition directory
-%                     condDir = strcat(partDir, "/", conditions(cond));
-%                     condFile = strcat(condDir, "/", Matfile);
-% 
-%                     % -- read the data captured for the test
-%                     TestData = load(condFile);
-% 
-%                     % -- get the end time
-%                     tf = TestData.simdata.time;
-% 
-%                     % -- plot everything nicely
-%                     subplot(2, 4, cond);
-%                     % -- display the Omron Lab map
-%                     hold on; imagesc([0 L(1)],[0 L(2)], TestData.img); 
-%                     set(gca,'ydir','reverse');
-%                     axis image;
-% 
-%                     % -- plot the target location
-%                     hold on; plot(TestData.simdata.tloc(1, 1), ...
-%                                   TestData.simdata.tloc(1, 2), ...
-%                                   'mx', 'LineWidth', 3, 'MarkerSize', 12); % -- Target location 
-% 
-%                     % -- plot the beginning and end points of the robots
-%                     hold on; plot(TestData.simdata.Xs(1, 1, 1, 1), ...
-%                                   TestData.simdata.Xs(2, 1, 1, 1), ...
-%                                   'ks', 'LineWidth', 3, 'MarkerSize', 12); % -- experiment robot (human controlled robot start)
-%                     hold on; plot(TestData.simdata.Xs(1, 1, 1, 2), ...
-%                                   TestData.simdata.Xs(2, 1, 1, 2), ...
-%                                   'rs', 'LineWidth', 3, 'MarkerSize', 12); % -- autonomous robot 1 start
-%                     hold on; plot(TestData.simdata.Xs(1, 1, 1, 3), ...
-%                                   TestData.simdata.Xs(2, 1, 1, 3), ...
-%                                   'gs', 'LineWidth', 3, 'MarkerSize', 12); % -- autonomous robot 2 start
-% 
-%                     hold on; plot(TestData.simdata.Xs(1, tf, 1, 1), ...
-%                                   TestData.simdata.Xs(2, tf, 1, 1), ...
-%                                   'kx', 'LineWidth', 3, 'MarkerSize', 12); % -- experiment robot (human controlled robot end)
-%                     hold on; plot(TestData.simdata.Xs(1, tf, 1, 2), ...
-%                                   TestData.simdata.Xs(2, tf, 1, 2), ...
-%                                   'rx', 'LineWidth', 3, 'MarkerSize', 12); % -- autonomous robot 1 end
-%                     hold on; plot(TestData.simdata.Xs(1, tf, 1, 3), ...
-%                                   TestData.simdata.Xs(2, tf, 1, 3), ...
-%                                   'gx', 'LineWidth', 3, 'MarkerSize', 12); % -- autonomous robot 2 end
-% 
-%                     % -- On top of the domain map, plot all trajectories of the robots
-%                     hold on; plot(TestData.simdata.Xs(1, 1:tf, 1, 1), ...
-%                                   TestData.simdata.Xs(2, 1:tf, 1, 1), ...
-%                                   'k-', 'LineWidth', 2); % -- experiment robot (human controlled robot trajectory)
-%                     hold on; plot(TestData.simdata.Xs(1, 1:tf, 1, 2), ...
-%                                   TestData.simdata.Xs(2, 1:tf, 1, 2), ...
-%                                   'r-', 'LineWidth', 2); % -- autonomous robot 1 trajectory
-%                     hold on; plot(TestData.simdata.Xs(1, 1:tf, 1, 3), ...
-%                                   TestData.simdata.Xs(2, 1:tf, 1, 3), ...
-%                                   'g-', 'LineWidth', 2); % -- autonomous robot 2 trajectory
-%                 end
-%                  i = i + 1;
-%             end
-%         end
-%     end
-% end
+for test = 4:4%1:size(files, 1)
+
+    % -- make sure that we capture a number not '.' or '..'
+    if (files(test).name ~= "." && files(test).name ~= "..")
+
+        % -- within the test number folder, get the participant numbers
+        subDir = strcat(parentDir, "/", files(test).name);
+        participant_folders = dir(subDir);
+
+        % -- begin looping through all the participant folders
+        for participant = 1:size(participant_folders, 1)
+            % -- make sure that we capture a number not '.' or '..'
+            if (participant_folders(participant).name ~= "." && participant_folders(participant).name ~= "..")
+                figure(participant-2); gcf;
+
+                % -- create participant directory
+                partDir = strcat(subDir,"/",participant_folders(participant).name);
+
+                % -- begin looping through all conditions tested
+                for cond = 1:size(conditions, 2)
+
+                    % -- create condition directory
+                    condDir = strcat(partDir, "/", conditions(cond));
+                    condFile = strcat(condDir, "/", Matfile);
+
+                    % -- read the data captured for the test
+                    TestData = load(condFile);
+
+                    % -- get the end time
+                    tf = TestData.simdata.time;
+
+                    % -- plot everything nicely
+                    subplot(2, 2, cond);
+                    % -- display the Omron Lab map
+                    hold on; imagesc([0 L(1)],[0 L(2)], TestData.img); 
+                    set(gca,'ydir','reverse');
+                    axis image;
+
+                    % -- plot the target location
+                    hold on; plot(TestData.simdata.tloc(1, 1), ...
+                                  TestData.simdata.tloc(1, 2), ...
+                                  'm*', 'LineWidth', 3, 'MarkerSize', 12); % -- Target location 
+
+                    % -- plot the beginning and end points of the robots
+                    hold on; plot(TestData.simdata.Xs(1, 1, 1, 1), ...
+                                  TestData.simdata.Xs(2, 1, 1, 1), ...
+                                  'ks', 'LineWidth', 3, 'MarkerSize', 12); % -- experiment robot (human controlled robot start)
+                    hold on; plot(TestData.simdata.Xs(1, 1, 1, 2), ...
+                                  TestData.simdata.Xs(2, 1, 1, 2), ...
+                                  'gs', 'LineWidth', 3, 'MarkerSize', 12); % -- autonomous robot 1 start
+                    hold on; plot(TestData.simdata.Xs(1, 1, 1, 3), ...
+                                  TestData.simdata.Xs(2, 1, 1, 3), ...
+                                  'gs', 'LineWidth', 3, 'MarkerSize', 12); % -- autonomous robot 2 start
+
+                    hold on; plot(TestData.simdata.Xs(1, tf, 1, 1), ...
+                                  TestData.simdata.Xs(2, tf, 1, 1), ...
+                                  'ko', 'LineWidth', 3, 'MarkerSize', 12); % -- experiment robot (human controlled robot end)
+                    hold on; plot(TestData.simdata.Xs(1, tf, 1, 2), ...
+                                  TestData.simdata.Xs(2, tf, 1, 2), ...
+                                  'go', 'LineWidth', 3, 'MarkerSize', 12); % -- autonomous robot 1 end
+                    hold on; plot(TestData.simdata.Xs(1, tf, 1, 3), ...
+                                  TestData.simdata.Xs(2, tf, 1, 3), ...
+                                  'go', 'LineWidth', 3, 'MarkerSize', 12); % -- autonomous robot 2 end
+
+                    % -- On top of the domain map, plot all trajectories of the robots
+                    hold on; plot(TestData.simdata.Xs(1, 1:tf, 1, 1), ...
+                                  TestData.simdata.Xs(2, 1:tf, 1, 1), ...
+                                  'k-', 'LineWidth', 2); % -- experiment robot (human controlled robot trajectory)
+                    hold on; plot(TestData.simdata.Xs(1, 1:tf, 1, 2), ...
+                                  TestData.simdata.Xs(2, 1:tf, 1, 2), ...
+                                  'g-', 'LineWidth', 2); % -- autonomous robot 1 trajectory
+                    hold on; plot(TestData.simdata.Xs(1, 1:tf, 1, 3), ...
+                                  TestData.simdata.Xs(2, 1:tf, 1, 3), ...
+                                  'g-', 'LineWidth', 2); % -- autonomous robot 2 trajectory
+                    title(sprintf("\\alpha_k (%s)", cond_names(cond)))          
+                end
+                 i = i + 1;
+            end
+        end
+    end
+end
+
+% -- Alpha plotting
+i = 1;
+
+
+% -- begin looping through each test folder
+for test = 1:10%1:size(files, 1)
+
+    % -- make sure that we capture a number not '.' or '..'
+    if (files(test).name ~= "." && files(test).name ~= "..")
+
+        % -- within the test number folder, get the participant numbers
+        subDir = strcat(parentDir, "/", files(test).name);
+        participant_folders = dir(subDir);
+
+        % -- begin looping through all the participant folders
+        for participant = 1:size(participant_folders, 1)
+            % -- make sure that we capture a number not '.' or '..'
+            if (participant_folders(participant).name ~= "." && participant_folders(participant).name ~= "..")
+                figure(participant+10); gcf;
+
+                % -- create participant directory
+                partDir = strcat(subDir,"/",participant_folders(participant).name);
+
+                % -- begin looping through all conditions tested
+                for cond = 1:size(conditions, 2)
+
+                    % -- create condition directory
+                    condDir = strcat(partDir, "/", conditions(cond));
+                    condFile = strcat(condDir, "/", Matfile);
+
+                    % -- read the data captured for the test
+                    TestData = load(condFile);
+
+                    % -- get the end time
+                    tf = TestData.simdata.time;
+                    alpha = TestData.simdata.alpha;
+
+                    % -- plot everything nicely
+                    subplot(2, 2, cond);
+
+                    % -- plot the target location
+                    hold on; plot(1:TestData.simdata.time, ...
+                                  TestData.simdata.alpha(1:tf), ...
+                                  'k-', 'LineWidth', 3); % -- Target location 
+
+                    title(sprintf("\\alpha_k (%s)", cond_names(cond)))          
+                end
+                 i = i + 1;
+            end
+        end
+    end
+end
+
+% --- 
+
 
 i = 1; 
 
@@ -208,7 +267,7 @@ for RWtest = 4:4%1:size(RW_files, 1)
                                   'ks', 'LineWidth', 3, 'MarkerSize', 12); % -- experiment robot (human controlled robot start)
                     hold on; plot(RWTestData.simdata.Xs(1, 1, 1, 2), ...
                                   RWTestData.simdata.Xs(2, 1, 1, 2), ...
-                                  'rs', 'LineWidth', 3, 'MarkerSize', 12); % -- autonomous robot 1 start
+                                  'gs', 'LineWidth', 3, 'MarkerSize', 12); % -- autonomous robot 1 start
                     hold on; plot(RWTestData.simdata.Xs(1, 1, 1, 3), ...
                                   RWTestData.simdata.Xs(2, 1, 1, 3), ...
                                   'gs', 'LineWidth', 3, 'MarkerSize', 12); % -- autonomous robot 2 start
@@ -218,7 +277,7 @@ for RWtest = 4:4%1:size(RW_files, 1)
                                   'kx', 'LineWidth', 3, 'MarkerSize', 12); % -- experiment robot (human controlled robot end)
                     hold on; plot(RWTestData.simdata.Xs(1, tf, 1, 2), ...
                                   RWTestData.simdata.Xs(2, tf, 1, 2), ...
-                                  'rx', 'LineWidth', 3, 'MarkerSize', 12); % -- autonomous robot 1 end
+                                  'gx', 'LineWidth', 3, 'MarkerSize', 12); % -- autonomous robot 1 end
                     hold on; plot(RWTestData.simdata.Xs(1, tf, 1, 3), ...
                                   RWTestData.simdata.Xs(2, tf, 1, 3), ...
                                   'gx', 'LineWidth', 3, 'MarkerSize', 12); % -- autonomous robot 2 end
@@ -229,11 +288,11 @@ for RWtest = 4:4%1:size(RW_files, 1)
                                   'k-', 'LineWidth', 2); % -- experiment robot (human controlled robot trajectory)
                     hold on; plot(RWTestData.simdata.Xs(1, 1:tf, 1, 2), ...
                                   RWTestData.simdata.Xs(2, 1:tf, 1, 2), ...
-                                  'r-', 'LineWidth', 2); % -- autonomous robot 1 trajectory
+                                  'g-', 'LineWidth', 2); % -- autonomous robot 1 trajectory
                     hold on; plot(RWTestData.simdata.Xs(1, 1:tf, 1, 3), ...
                                   RWTestData.simdata.Xs(2, 1:tf, 1, 3), ...
                                   'g-', 'LineWidth', 2); % -- autonomous robot 2 trajectory
-                    title(sprintf("Random Walk scenario\n Condition: %d", cond))
+                    title(sprintf("random (%s)", cond_names(cond)))
     
                 end
                  i = i + 1;
@@ -242,6 +301,7 @@ for RWtest = 4:4%1:size(RW_files, 1)
     end
 end
 end
+
 
 function PlotRWControls()
 
@@ -373,7 +433,7 @@ conditions = ["condition_1",...
 Matfile = "OmronLab_p=1200_nsim=1_agents=3.mat";
 
 % -- pull list of test folders within test folder
-parentDir = "data/alpha_t/TotalDist";
+parentDir = "data/alpha_t/FreezeTime";%TotalDist,FreezeTime
 parentDir_RW = "data/RandomWalk";
 parentDir0 = "data/alpha_0";
 parentDir1 = "data/alpha_1";
@@ -384,6 +444,7 @@ files0 = dir(parentDir0);
 files1 = dir(parentDir1);
 
 timeArray = csvread('../data/TimeToFind.csv');
+timeArray(:,1:4) = timeArray(:,1:4)-5.0; % -- adjusting for humans entering code into computer
 IDs = csvread('../src/IDList_Completed.csv', 1);
 
 % -- initialize the counter
